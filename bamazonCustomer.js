f8dfab2a37f1bcbd973062f87f88d0d332ce95d7
Shehryar Ehtesham <se318@rutgers.edu>
2019-11-14 18:49:59 -0500

    Connected SQL to Js file and started inquire method

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,43 +3,65 @@ var inquirer = require("inquirer");
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "",
-  database: "greatBay_DB"
+    // Your password
+    password: "Applejuice670",
+    database: "greatBay_DB"
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
-  if (err) throw err;
-  // run the start function after the connection is made to prompt the user
-  start();
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
 });
 
 function start() {
     inquirer
-      .prompt({
-        name: "postOrBid",
-        type: "list",
-        message: "Would you like to [POST] an auction or [BID] on an auction?",
-        choices: ["POST", "BID", "EXIT"]
-      })
-      .then(function(answer) {
-        // based on their answer, either call the bid or the post functions
-        if (answer.postOrBid === "POST") {
-          postAuction();
-        }
-        else if(answer.postOrBid === "BID") {
-          bidAuction();
-        } else{
-          connection.end();
-        }
-      });
-  }
+        .prompt({
+            name: "IDNumber",
+            type: "number",
+            message: "What is the ID number of the product you would like to buy?",
+            choices: Number
+        })
+        .then(function (answer) {
+            // based on their answer, either call the bid or the post functions
+            if (answer.IDNumber === Number) {
+                nextQuestion();
+            }
+            else if (answer.IDNumber === null) {
+                tryagain()
+            } else {
+                connection.end();
+            }
+        });
+}
+
+function nextQuestion() {
+    inquirer
+        .prompt({
+            name: "Quantity",
+            type: "number",
+            message: "How many units of the product would you like to buy?",
+            choices: Number
+        })
+        .then(function (answer) {
+            if (answer.Quantity === Number) {
+                placeOrder()
+            } else {
+                console.log(`Insufficient quantity!`);
+            }
+        })
+}
+
+
+function tryagain(){
+    console.log("Please Try Again")
+}
